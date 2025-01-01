105a2d9a868818286bbed12c9b200f8886472c3d
limepop <0limepop0@gmail.com>
2024-12-31 23:08:53 -0800

    Initial commit

---
+++ src/client/java/net/limepop/limeorigins/LimeOriginsDataGenerator.java
@@ -0,0 +1,11 @@
+package net.limepop.limeorigins;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LimeOriginsDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
